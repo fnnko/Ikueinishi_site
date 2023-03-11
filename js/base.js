3fd859e7de17af0839c5d0d9034e048fdcb3a3b9
fnnko <fuuka_smile@icloud.com>
2023-03-11 11:35:42 +0900

    スピード変更

--- js/base.js
+++ js/base.js
@@ -101,7 +101,7 @@ var swiper = new Swiper(".mySwiper", {
 
   loop: true, // ループ有効
   slidesPerView: 2, // 一度に表示する枚数
-  speed: 6000, // ループの時間
+  speed: 3000, // ループの時間
   allowTouchMove: false, // スワイプ無効
   autoplay: {
     delay: 0, // 途切れなくループ
